894fb5720addf8d33abe182161cce5d212f4d60e
Dilkush Singh <work.dilkushsingh@gmail.com>
2024-08-05 15:18:35 +0530

    Add plot functionality for india

--- app.py
+++ app.py
@@ -3,6 +3,8 @@ import pandas as pd
 import streamlit as st
 import plotly.express as px
 
+st.set_page_config(layout='wide')
+
 df = pd.read_csv('census_2011_cleaned.csv')
 list_of_states = list(df['State'].unique())
 list_of_states.insert(0, 'India')
@@ -15,3 +17,10 @@ primary = st.sidebar.selectbox('Select Primary Parameter', list(df.columns[4:]))
 secondary = st.sidebar.selectbox('Select Secondary Parameter', list(df.columns[4:]))
 
 plot = st.sidebar.button('Analyze')
+
+if plot:
+    if selected_state == 'India':
+        fig = px.scatter_mapbox(df, lat='Latitude', lon='Longitude', size=primary, color=secondary, zoom=3.5, size_max=35, mapbox_style='carto-positron', width=1080, height=700)
+        st.plotly_chart(fig, use_container_width=True)
+    else:
+        pass
