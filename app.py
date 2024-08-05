f47c930b84c64ee3f17dd6e6f0d5a1e859e1784a
Dilkush Singh <work.dilkushsingh@gmail.com>
2024-08-05 16:03:52 +0530

    Add final changes

--- app.py
+++ app.py
@@ -1,15 +1,16 @@
-import numpy as np
 import pandas as pd
 import streamlit as st
 import plotly.express as px
 
-st.set_page_config(layout='wide')
+st.set_page_config(page_title='Census 2011 Analysis', layout='wide')
 
 df = pd.read_csv('census_2011_cleaned.csv')
+
 list_of_states = list(df['State'].unique())
 list_of_states.insert(0, 'India')
 
-st.sidebar.title('India Census 2011 Analysis')
+st.title('India Census 2011 Analysis')
+st.sidebar.title('Select Criterias')
 
 selected_state = st.sidebar.selectbox('Select State', list_of_states)
 
@@ -22,9 +23,12 @@ if plot:
     st.text('Size represent Primary parameter.')
     st.text('Color represent Secondary parameter.')
     if selected_state == 'India':
-        fig = px.scatter_mapbox(df, lat='Latitude', lon='Longitude', size=primary, color=secondary, zoom=3.5, size_max=35, mapbox_style='carto-positron', width=1080, height=700, hover_name='District')
+        fig = px.scatter_mapbox(df, lat='Latitude', lon='Longitude', size=primary, color=secondary, zoom=3.5, size_max=35, mapbox_style='carto-positron', color_continuous_scale=px.colors.sequential.Plasma, width=1200, height=700, hover_name='District')
         st.plotly_chart(fig, use_container_width=True)
     else:
         state_df = df[df['State'] == selected_state]
-        fig = px.scatter_mapbox(state_df, lat='Latitude', lon='Longitude', size=primary, color=secondary, zoom=5, size_max=35, mapbox_style='carto-positron', width=1080, height=700, hover_name='District')
+        fig = px.scatter_mapbox(state_df, lat='Latitude', lon='Longitude', size=primary, color=secondary, zoom=5, size_max=35, mapbox_style='carto-positron', color_continuous_scale=px.colors.sequential.Plasma, width=1200, height=700, hover_name='District')
         st.plotly_chart(fig, use_container_width=True)
+
+st.text('For Contacting me, or checking out more projects')
+st.markdown("[Visit Portfolio](https://dilkushsingh.github.io)")
