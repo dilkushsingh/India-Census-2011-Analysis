f26857db060d703ac959d619d2caee1b39846ea8
Dilkush Singh <work.dilkushsingh@gmail.com>
2024-08-06 10:16:50 +0530

    Add functionality of select India by default

--- app.py
+++ app.py
@@ -17,15 +17,14 @@ selected_state = st.sidebar.selectbox('Select State', list_of_states)
 primary = st.sidebar.selectbox('Select Primary Parameter', list(df.columns[4:]))
 secondary = st.sidebar.selectbox('Select Secondary Parameter', list(df.columns[4:]))
 
-plot = st.sidebar.button('Analyze')
-
-if plot:
-    st.text('Size represent Primary parameter.')
-    st.text('Color represent Secondary parameter.')
-    if selected_state == 'India':
-        fig = px.scatter_mapbox(df, lat='Latitude', lon='Longitude', size=primary, color=secondary, zoom=3.5, size_max=35, mapbox_style='carto-positron', color_continuous_scale=px.colors.sequential.Plasma, width=1200, height=700, hover_name='District')
-        st.plotly_chart(fig, use_container_width=True)
-    else:
+st.text('Size represent Primary parameter.')
+st.text('Color represent Secondary parameter.')
+if selected_state == 'India':
+    fig = px.scatter_mapbox(df, lat='Latitude', lon='Longitude', size=primary, color=secondary, zoom=3.5, size_max=35, mapbox_style='carto-positron', color_continuous_scale=px.colors.sequential.Plasma, width=1200, height=700, hover_name='District')
+    st.plotly_chart(fig, use_container_width=True)
+else:
+    plot = st.sidebar.button('Analyze')
+    if plot:
         state_df = df[df['State'] == selected_state]
         fig = px.scatter_mapbox(state_df, lat='Latitude', lon='Longitude', size=primary, color=secondary, zoom=5, size_max=35, mapbox_style='carto-positron', color_continuous_scale=px.colors.sequential.Plasma, width=1200, height=700, hover_name='District')
         st.plotly_chart(fig, use_container_width=True)
